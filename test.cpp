b17ad43982e12a7a77e4d2ba5e47faa0b6bee1c6
Koishiiii <1544758259@qq.com>
2020-12-24 22:57:17 +0800

    append Git

--- test.cpp
+++ test.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Git!Welcome to Git!" << endl;
+    cout << "Hello Git!Welcome to Git!Git is a distributed version control system." << endl;
     return 0;
 }
